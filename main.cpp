c8040666e81c21ff4e2b2d83f5170cc49a909544
川原大宙 <k_hiro@kzd.biglobe.ne.jp>
2021-11-13 22:11:43 +0900

    メンバーiのスキル合計から平均値を求めてランダムに増減させる

--- main.cpp
+++ main.cpp
@@ -8,7 +8,8 @@ long long mod = 1e9 + 7;
 
 int dx[4] = {1, 0, -1, 0};
 int dy[4] = {0, 1, 0, -1};
-
+int skillNum = 0;
+int memberNum = 0;
 #define debug 0
 #define debugWorkTime 0
 #define debugWorkTimeMax 1
@@ -20,12 +21,18 @@ int dy[4] = {0, 1, 0, -1};
 #define useSample 0
 #define useTestDate 0
 #define setRandonSelect 0
-
+int days = 0;
 vector<vector<int>> from(1010);
+vector<vector<int>> to(1010);
 
 // taskSkill[i][k]:=タスクiに必要な技能k
 double taskSkill[1010][25];
 
+// skillSum[i]:=タスクiの必要な技能の合計値
+double skillSum[1010];
+
+// PreDoneTask[i]:=メンバーiが直前にやったタスクにかかった時間とそのタスク番号
+vector<pair<double, int>> PreDoneTask(25, make_pair(1e5, -1));
 // assing[i]:=メンバーiに割り振られたタスク、タスクがなければ-1を入れる
 vector<long long> taskAssign(25);
 
@@ -41,6 +48,13 @@ bool finished[1010] = {};
 // 進行中かどうか、進行中ならtrue
 bool onGoing[1010] = {};
 
+
+// estimatedSkill[i][p]:=メンバーiのスキルpの推定値
+double estimatedSkill[1010][25];
+
+//メンバ-iが現在やっているタスクにかかる時間の推定値
+vector<double>estTime(25,3.0);
+
 // 現在そのタスクに取り組むことができるか
 bool availableTask[1010];
 
@@ -55,6 +69,9 @@ vector<pair<double, int>> sortTaskByL2norm;
 
 bool cmp(pair<double, int> p1, pair<double, int> p2) {
   return p1.first < p2.first;
+}
+bool cmpInv(pair<double, int> p1, pair<double, int> p2) {
+  return p1.first > p2.first;
 }
 //現在のタスクにかかっている時間
 vector<int> workingTime(25, 0);
@@ -106,20 +123,42 @@ void sortTasks(int N, int K) {
   sort(sortTaskByL2norm.begin(), sortTaskByL2norm.end(), cmp);
   return;
 }
+//
+void sortTasksByTo(int N) {
+  rep(i, N) sortTaskByL2norm.push_back(make_pair(to[i].size(), i));
+  sort(sortTaskByL2norm.begin(), sortTaskByL2norm.end(), cmp);
+}
+std::random_device rnd;  // 非決定的な乱数生成器を生成
+std::mt19937 mt(
+    rnd());  //  メルセンヌ・ツイスタの32ビット版、引数は初期シード値
+//メンバーiがタスクkについてtime時間かかった時の推定
+void estSkill(int i, int k, int time) {
+  std::uniform_real_distribution<> rand(
+      0, skillNum);  // [0.0,skillNum] 範囲の一様乱数
+  rep(l, skillNum) {
+    if(abs(estTime[i]-time)<5){
+      continue;
+    }
+    else if(abs(estTime[i]-time)>20)estimatedSkill[i][l] = taskSkill[k][l]+rand(mt)/skillNum;
+    else{
+      estimatedSkill[i][l]=(estimatedSkill[i][l]*completedTask[i]+taskSkill[k][l])/(completedTask[i]+1.0);
+    }
+  }
+  //ランダムに選択した項目iついて、平均から-1.0するする
+  rep(l, time/2.0) {
+    int skillIndex = rand(mt);
+    
+    //メンバーiがタスクkについてtime時間かかった時の推定
+    estimatedSkill[i][skillIndex] -=  2.0;//3*rand(mt)/skillNum;
+    estimatedSkill[i][skillIndex]=max(0.0,estimatedSkill[i][skillIndex]);
+  }
+  return;
+}
 //メンバーを格納する配列
 vector<MemberInfo> Members(25);
 
 //ソートの比較関数
 bool sortMembers(MemberInfo m1, MemberInfo m2) { return true; }
-
-//ソートの比較関数 小さい順にソート
-// bool cmp(pair<double, int> p1, pair<double, int> p2) {
-//   return p1.first < p2.first;
-// }
-// //降順にソートする
-// bool sortByDown(pair<double, int> p1, pair<double, int> p2) {
-//   return p1.first < p2.first;
-// }
 
 void makeSelectOrder(int m) {
   rep(i, m) selectOrder[i] = make_pair(0.5, i);
@@ -128,6 +167,8 @@ void makeSelectOrder(int m) {
 //タスクの類似度を降順にmapで管理
 vector<multimap<double, int>> taskSimByDown(1010);
 
+//あるメンバーの推定スキル平均
+double skillAve[1010];
 //タスクxとタスクyの類似度を求める
 // 類似度=cos類似度とする
 double getSim(int x, int y, int k) {
@@ -138,6 +179,8 @@ double getSim(int x, int y, int k) {
     ySumSq += taskSkill[y][i];
     xySum += taskSkill[x][i] * taskSkill[y][i];
   }
+  skillSum[x] = xSumSq;
+  skillSum[y] = ySumSq;
   xSumSq = sqrt(xSumSq);
   ySumSq = sqrt(ySumSq);
   return xySum / (xSumSq * ySumSq);
@@ -166,7 +209,7 @@ void makeDependency(int R) {
     u--, v--;
     //依存関係をtoとfromで表現
     // to[1]=2　の時1->２の依存関係がある
-    // to[u].push_back(v);
+    to[u].push_back(v);
     // from[2]=1の時2->の依存関係がある
     from[v].push_back(u);
   }
@@ -201,13 +244,75 @@ bool taskIsReady(int k) {
   return true;
 }
 
+double getMemberSkillSum(int member, int nowTask, double nowTime) {
+  int preTask = PreDoneTask[member].second;
+  if (preTask < 0) return 1.0;
+
+  double preTime = PreDoneTask[member].first;
+  return (skillSum[preTask] + skillSum[nowTask] - (preTime + nowTime)) / 2.0;
+}
 //  メンバーiの仕事が終わったことを記録
 void setFinish(int i) {
   //担当していたタスクの終了を記録
   finished[taskAssign[i]] = true;
   //進行中フラグを閉じる
   // onGoing[taskAssign[i]] = false;
-  
+  rep(k, 25) {
+    if (selectOrder[k].second == i) {
+      //今までの作業にかかった時間の最大値で更新
+      // selectOrder[k].first = max(selectOrder[k].first, workingTime[i]);
+
+      //今までの作業にかかった時間の最小値で更新
+      if (selectOrder[k].first == 0.0)
+        selectOrder[k].first = (double)workingTime[i];
+      else {
+#if debugWorkTimeMin
+
+        selectOrder[k].first =
+            min(selectOrder[k].first, (double)workingTime[i]);
+#elif 0  // debugWorkTimeMax
+
+        selectOrder[k].first =
+            max(selectOrder[k].first, (double)workingTime[i]);
+#elif sortByMaxMinAve
+        //最小値
+        Members[i].WorkTimeMin =
+            min(Members[i].WorkTimeMin, (double)workingTime[i]);
+        //平均値
+        Members[i].WorkTimeAve =
+            (double)(Members[i].WorkTimeAve * completedTask[i] +
+                     workingTime[i]) /
+            (completedTask[i] + 1.0);
+        //最大値
+        Members[i].WorkTimeMax =
+            max(Members[i].WorkTimeMax, (double)workingTime[i]);
+        // selectOrder[k].first = Members[i].getRateValue();
+        double skilllSumNow =
+            getMemberSkillSum(i, taskAssign[i], workingTime[i]);
+        selectOrder[k].first =
+            Members[i].getRateValue();  // / (skilllSumNow + 1);
+        // printf("# skillSum member %d = %lf\n",i,getMemberSkillSum(i,
+        // taskAssign[i], workingTime[i]));
+        estSkill(i, taskAssign[i], workingTime[i]);
+        // printf("#s %d", i + 1);
+        // rep(p, skillNum) printf(" %lf", estimatedSkill[i][p]);
+        // printf("\n");
+        
+        
+
+        PreDoneTask[i] = make_pair((double)workingTime[i], taskAssign[i]);
+
+#if 0
+        // debug用
+        printf("# member %d (Max %lf Min %lf Ave %lf Rate =%lfM )\n",i, Members[i].WorkTimeMax,
+               Members[i].WorkTimeMin, Members[i].WorkTimeAve,
+               Members[i].getRateValue());
+#endif
+#endif
+      }
+      break;
+    }
+  }
 
   //タスクにかかった時間を記録する
   finishTimeAndMember[i].insert(make_pair(workingTime[i], taskAssign[i]));
@@ -225,7 +330,7 @@ void setFinish(int i) {
 // メンバーi タスク k
 bool getTask(int i, int n) {
   for (int l = 0; l < n; l++) {
-    int k = sortTaskByL2norm[l].second;
+    int k = l;  //= sortTaskByL2norm[l].second;
     //タスクkが開始可能であれば割り当てる
     if (taskIsReady(k)) {
       taskAssign[i] = k;
@@ -327,22 +432,22 @@ int getOptTask(int i) {
   int taskIndex;
   auto it = finishTimeAndMember[i].begin();
   //メンバーiがやったタスクについてみていく
-  //while (it != finishTimeAndMember[i].end()) {
-    // if (it->first > 10.0) {
-    //   it++;
-    //   continue;
-    // }
-    taskIndex = it->second;
-    // task番号==taskIndex のタスクについて類似度の高いものを取ってくる
-    auto itSim = taskSimByDown[taskIndex].rbegin();
-    while (itSim != taskSimByDown[taskIndex].rend()) {
-      //類似度が高いものを返す
-      if (taskIsReady(itSim->second)) {
-        return itSim->second;
-      }
-      itSim++;
+  // while (it != finishTimeAndMember[i].end()) {
+  // if (it->first > 10.0) {
+  //   it++;
+  //   continue;
+  // }
+  taskIndex = it->second;
+  // task番号==taskIndex のタスクについて類似度の高いものを取ってくる
+  auto itSim = taskSimByDown[taskIndex].rbegin();
+  while (itSim != taskSimByDown[taskIndex].rend()) {
+    //類似度が高いものを返す
+    if (taskIsReady(itSim->second)) {
+      return itSim->second;
     }
-    it++;
+    itSim++;
+  }
+  it++;
   //}
   return -1;
 }
@@ -357,25 +462,86 @@ void registTaskToMember(int member, int task) {
 //似たタスクを早く終わらせた人に割り当てる
 void assignByTaskSimilarity(int m, int n) {
   vector<pair<int, int>> output;  //出力用ベクトル
+  sort(selectOrder.begin(), selectOrder.begin() + m, cmp);
 
   //空いているメンバーについて割り当てる
-  rep(i, m) {
-    if (taskAssign[i] >= 0)
-      continue;
-    else {
-      //メンバーiについて、
-      //過去に行ったタスクと類似度が高いタスクを取ってくる
-      int taskCand = getOptTask(i);
-      if (taskCand > -1) {
-        registTaskToMember(i, taskCand);
-        output.push_back(make_pair(i, taskCand));
-      } else {
-        if (getTask(i, n)) {
-          // registTaskToMember(i, );
-          output.push_back(make_pair(i, taskAssign[i]));
-          break;
-        }
+  rep(l, m) {
+    int i = l;
+    // i= selectOrder[l].second;
+    if (taskAssign[i] >= 0) continue;
+
+    //メンバーiについて、
+    //過去に行ったタスクと類似度が高いタスクを取ってくる
+    int taskCand = -1;
+    if (days > 500) taskCand = getOptTask(i);
+    if (taskCand > -1) {
+      registTaskToMember(i, taskCand);
+      output.push_back(make_pair(i, taskCand));
+    } else {
+      if (getTask(i, n)) {
+        // registTaskToMember(i, );
+        output.push_back(make_pair(i, taskAssign[i]));
+        // break;
       }
+    }
+  }
+  printf("%d ", output.size());
+  for (auto now : output) {
+    printf("%d %d ", now.first + 1, now.second + 1);
+  }
+  printf("\n");
+  return;
+}
+double getDiffSum(int i, int l, int k) {
+  double ret = 0.0;
+  rep(p, k) ret += max(0.0, taskSkill[l][p] - estimatedSkill[i][p]);
+  return ret;
+}
+
+//誤差が最も小さいものを取ってくる
+pair<int, double> getTaskByDiff(int i, int k, int n) {
+  double minDiff = 1e5;
+  int retTask = -1;
+  rep(l, n) {
+    if (taskIsReady(l)) {
+      // if (skillAve[i] = 0.0) return l;
+      double nowDiff = getDiffSum(i, l, k);
+      if (nowDiff < minDiff) {
+        retTask = l;
+        minDiff = nowDiff;
+      }
+    }
+  }
+  return make_pair(retTask, minDiff);
+}
+
+//メンバーのスキルの推定値との差が最も小さいタスクを割り当てる
+void assignByTaskDiff(int m, int n) {
+  vector<pair<int, int>> output;  //出力用ベクトル
+  sort(selectOrder.begin(), selectOrder.begin() + m, cmp);
+
+  //空いているメンバーについて割り当てる
+  rep(l, m) {
+    int i = l;
+    i = selectOrder[l].second;
+    if (taskAssign[i] >= 0) continue;
+    //メンバーiについて、
+    int nextTask = -1;
+    double cost = 1e5;
+    //3回やって最も良い時を採用
+    rep(_, 1) {
+      //estSkill(i, PreDoneTask[i].second, PreDoneTask[i].first);
+      auto taskAndDay = getTaskByDiff(i, skillNum, n);
+      if (taskAndDay.second < cost) {
+        cost = taskAndDay.second;
+        nextTask = taskAndDay.first;
+      }
+    }
+    if (nextTask > -1) {
+      estTime[i]=cost;
+      registTaskToMember(i, nextTask);
+      output.push_back(make_pair(i, taskAssign[i]));
+      // break;
     }
   }
   printf("%d ", output.size());
@@ -470,11 +636,15 @@ int main() {
   // updateAvalableTask(N);
 
   //タスクをL2ノルムが小さい順にソート
-  sortTasks(N, K);
-  // //初日の割り当て
-  assignByTaskSimilarity(M, N);
+  // sortTasks(N, K);
 
-  int days = 0;
+  // sortTasksByTo(N);
+  //  //初日の割り当て
+  // assignByTaskSimilarity(M, N);
+  assignByTaskDiff(M, N);
+
+  skillNum = K;
+  memberNum = M;
   while (1) {
     days++;
     //メンバーの労働時間を記録
@@ -506,7 +676,9 @@ int main() {
     // simpleAssign(M, N);
 
     //タスクをタスク間の類似度に基づいて分類
-    assignByTaskSimilarity(M, N);
+    // assignByTaskSimilarity(M, N);
+
+    assignByTaskDiff(M, N);
   }
   // clock_t end = clock();  // 終了時間
   // printf("#duration = %lf\n", (double)(end - start) / CLOCKS_PER_SEC);
